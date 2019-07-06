20af67d5d4ac1bbf6b1e358ac4b2d9c3802dab48
Wellington <well.raiser@gmail.com>
2019-07-06 11:55:06 -0300

    Versão 1 solução como encontrar o inverso de qualquer nuumero

---
+++ Ask-anyone-reverse-number-byWell/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ask_anyone_reverse_number_byWell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char r = 's';
+            Console.WriteLine("Digite um numero para encontrar o inverso dele:");
+            while (r == 's')
+            {
+                Console.Write("Digite:");
+                double n = double.Parse(Console.ReadLine());
+                if (n == 0)
+                {
+                    Console.WriteLine("Zero não é possivel encontrar o inverso!");
+                }
+                else
+                {
+                    Console.WriteLine($"O inverso de {n} é: " + 1 / n);
+                }
+                Console.WriteLine("Deseja continuar? (s/n)");
+                r = char.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+    }
+}//by Wellington Henrique Moreira Vitorino.
